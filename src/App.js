265dcae98c4c72c399484540e41ce1a435f5df33
Henry Chen <53919234+hen-chen@users.noreply.github.com>
2021-10-14 23:14:31 -0400

    Post

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+ 
+import Title from './components/Title'
+const App = () => {
+
+  return (
+    <>
+      <Title />
+      <h1> hi </h1>
+    </>
+  )
+}
+export default App
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Title = () => <h1>React Community App</h1>
+
+export default Title
