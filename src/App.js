39a50bc3b7bbd254bfeb539ad43e15e3f1732e6f
Henry Chen <53919234+hen-chen@users.noreply.github.com>
2021-10-18 00:22:53 -0400

    posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react'
  
 import Title from './components/Title'
-const App = () => {
+import NewPost from './components/NewPost'
+import Posts from './components/Posts'
+import './App.css'
 
+const App = () => {
+  const [posts, setPosts] = useState([])
   return (
     <>
-      <Title />
-      <h1> hi </h1>
+      <div className="center">
+        <Title />
+        <NewPost setPosts={setPosts} posts={posts}/>
+        <Posts posts={posts}/>
+      </div>
     </>
   )
 }
